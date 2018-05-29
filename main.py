4d82a7c23061b4e8c95e50e65951f20a23ce738b
rob <rob@rob.com>
2018-05-29 23:08:04 +0100

    Changed main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
-print("Hello world. My name is Roberto and I am 29 years old!!")
+def main(msg):
+    print (msg)
+
+main("Hello world. My name is Roberto and I am 29 years old!!")
